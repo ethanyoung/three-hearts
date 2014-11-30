e7dea940d0a9bef9b20422ba9c94b795964bd993
Ethan Yang <ethan.y@outlook.com>
2014-11-30 19:41:42 +1100

    Use Arcade system

--- assets/main.js
+++ assets/main.js
@@ -2,25 +2,27 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game-screen', { preload: pr
 
 function preload() {
     game.load.tilemap('map', 'assets/tilemaps/maps/collision_test.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('hero', 'assets/hero.png');
+    game.load.image('player', 'assets/player.png');
     game.load.image('ground_1x1', 'assets/tilemaps/tiles/walls_1x1.png');
     game.load.image('walls_1x2', 'assets/tilemaps/tiles/walls_1x1.png');
     game.load.image('tiles2', 'assets/tilemaps/tiles/walls_1x1.png');
     game.load.image('heart', 'assets/heart.png');
+    game.load.image('enemy', 'assets/enemy.png');   
 }
 
-var hero;
+var player;
 var cursors;
 var map;
 var hearts;
 var text;
+var enemies;
 
 function create() {
     score = 0;
 
-    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.stage.backgroundColor = '#018E0E';
-    game.physics.p2.defaultRestitution = 0.8;
+    // game.physics.p2.defaultRestitution = 0.8;
 
     map = game.add.tilemap('map');
 
@@ -31,23 +33,25 @@ function create() {
     layer = map.createLayer('Tile Layer 1');
     layer.resizeWorld();
     map.setCollisionBetween(1, 12);
-    game.physics.p2.convertTilemap(map, layer);
 
-	hero = game.add.sprite(200, 200, 'hero');
-	game.physics.p2.enable(hero);
-    hero.body.setZeroDamping();
-    hero.body.fixedRotation = true;
+	player = game.add.sprite(200, 200, 'player');
+	game.physics.enable(player);
+    player.body.fixedRotation = true;
 
     hearts = game.add.group();
     hearts.enableBody = true;
-    hearts.physicsBodyType = Phaser.Physics.P2JS;
+    hearts.physicsBodyType = Phaser.Physics.ARCADE;
     hearts.create(500, 500, 'heart');
     hearts.create(300, 150, 'heart');
     hearts.create(600, 100, 'heart');
 
-    game.camera.follow(hero);
+    enemies = game.add.group();
+    enemies.enableBody = true;
+    enemies.physicsBodyType = Phaser.Physics.ARCADE;
+    var enemy = enemies.create(600, 200, 'enemy');
+    enemy.body.velocity.y = 200;
 
-    game.physics.p2.setBoundsToWorld(true, true, true, true, false);
+    game.camera.follow(player);
 
 	cursors = game.input.keyboard.createCursorKeys();
 
@@ -55,24 +59,29 @@ function create() {
 }
 
 function update() {
-	hero.body.setZeroVelocity();
+    game.physics.arcade.collide(player, layer);
+    game.physics.arcade.collide(enemies, layer);
+    game.physics.arcade.collide(hearts, layer);
+
+	player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
 
     if (cursors.left.isDown)
     {
-    	hero.body.moveLeft(200);
+    	player.body.velocity.x = -200;
     }
     else if (cursors.right.isDown)
     {
-    	hero.body.moveRight(200);
+    	player.body.velocity.x = 200;
     }
 
     if (cursors.up.isDown)
     {
-    	hero.body.moveUp(200);
+    	player.body.velocity.y = -200;
     }
     else if (cursors.down.isDown)
     {
-    	hero.body.moveDown(200);
+    	player.body.velocity.y = 200;
     }
 
     checkHearts();
@@ -84,7 +93,7 @@ function render() {
 
 function checkHearts() {
     hearts.forEach(function(heart){
-        if (checkOverlap(hero, heart)) {
+        if (checkOverlap(player, heart)) {
             score += 1;
             text.text = 'You got ' + score + ' heart!';
             heart.kill();
